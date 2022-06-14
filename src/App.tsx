830b44b95cad391f9242337d7ca710609a151b3e
Cesar Roman <croman@twilio.com>
2022-06-14 17:01:14 -0400

    fixed weird paste/typescrippt bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-
 import * as React from 'react';
-
-
+import Login from './Login';
+import {Theme} from '@twilio-paste/core/theme';
 interface Props {
    name: string
 }
-
 class App extends React.Component<Props> {
   render() {
     const { name } = this.props;
     return (
-      <>
+      <Theme.Provider theme="default">
         <h1>
           Hello {name}
         </h1>
-      </>
+        <Login />
+      </Theme.Provider>
     );
   }
 }
-
 export default App;
