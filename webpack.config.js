aff0ff06969f2df5737bafb777cc3a7f875a59dd
Cesar Roman <croman@twilio.com>
2022-06-16 14:13:58 -0400

    added logo to header made changes to webpack, gitignore, and styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require("path");
 
 const config = {
-  entry: [
-
-    './src/index.tsx'
-  ],
+  entry: ["./src/index.tsx"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
-    'static': {
-      directory: './dist'
-    }
+    static: {
+      directory: "./dist",
+    },
   },
   resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
-      ]
-  }
+    extensions: [".tsx", ".ts", ".js"],
+  },
 };
 
 module.exports = config;
