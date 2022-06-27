e7b671ffbbd4a0fbd82f19dc5ee82315e93d524d
Elias Clark <eclark@twilio.com>
2022-06-27 18:13:25 -0400

    DASHBOARD styling (mostly) completed

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Heading, Anchor } from "@twilio-paste/core";
 import { Box } from "@twilio-paste/core/box";
+import { FileIcon } from "@twilio-paste/icons/esm/FileIcon";
 import {
   DataGrid,
   DataGridHead,
@@ -10,12 +12,14 @@ import {
   DataGridHeaderSort,
   DataGridBody,
   DataGridCell,
-  DataGridFoot
+  DataGridFoot,
 } from "@twilio-paste/core/data-grid";
 import type { SortDirection } from "@twilio-paste/core/data-grid";
+import "./styles.css";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  const userID: number = 1;
+  const userID: number = 2;
   const baseURL: string = "http://localhost:3000/";
   const [evaluations, setEvaluations] = useState([
     {
@@ -33,6 +37,14 @@ const Dashboard = () => {
     },
   ]);
 
+  const HeaderData: any = [
+    "Title",
+    "Date Submitted",
+    "Apprentice",
+    "Manager",
+    "Reviewers",
+  ];
+
   useEffect(() => {
     axios.get(baseURL + "evaluations").then((response) => {
       setEvaluations(
@@ -45,21 +57,86 @@ const Dashboard = () => {
     });
   }, []);
 
+  function Status(evaluation : any) {
+    if (evaluation.status === "open") {
+      return <Box id="openBox">Open</Box>
+    } else if (evaluation.status === "in progress") {
+      return <Box id="inProgressBox">In Progress</Box>
+    } else { 
+      return <Box id="completedBox">Completed</Box>
+    }
+  };
   
+
   return (
-    <div>
-      {evaluations.map((evaluation) => (
-        <div key={evaluation.title}>
-          {" "}
-          {evaluation.title} -{evaluation.creation} -{"Apprentice: "}{" "}
-          {evaluation.apprentice.name} -{" Status:"} {evaluation.status} -
-          {"Manager: "}
-          {evaluation.manager.name} -{"Reviewers: "}
-          {evaluation.reviews.map((review) => (
-            <span key={review.reviewId}> {review.reviewer} </span>
-          ))}{" "}
-        </div>
-      ))}
+    <div id="dashboard">
+      <div id="filter">
+        <Box>
+          <Heading as="h1" variant="heading50">
+            {" "}
+            Filters{" "}
+          </Heading>
+          <p> Sort by: </p>
+          <p> Uh some stuff here ????</p>
+        </Box>
+      </div>
+      <DataGrid aria-label="Evaluations list" data-testid="data-grid" striped>
+        <DataGridHead>
+          <DataGridRow>
+            <DataGridHeader width="200px">Evaluation Title</DataGridHeader>
+            <DataGridHeader width="150px">Date Initialized</DataGridHeader>
+            <DataGridHeader width="150px">Apprentice</DataGridHeader>
+            <DataGridHeader width="150px">Manager</DataGridHeader>
+            <DataGridHeader width="200px">Reviewers</DataGridHeader>
+            <DataGridHeader width="100px">Status</DataGridHeader>
+            <DataGridHeader width="180px">Details</DataGridHeader>
+          </DataGridRow>
+        </DataGridHead>
+        <DataGridBody>
+          {evaluations.map((evaluation) => (
+            <DataGridRow key={evaluation.title}>
+              <DataGridCell>
+                <Heading as="h2" variant="heading50">
+                  {evaluation.title}
+                </Heading>
+              </DataGridCell>
+              <DataGridCell>{evaluation.creation}</DataGridCell>
+              <DataGridCell>{evaluation.apprentice.name}</DataGridCell>
+              <DataGridCell>{evaluation.manager.name}</DataGridCell>
+              <DataGridCell>
+                <ul className="list">
+                  {evaluation.reviews.map((review) => (
+                    <li className="listItems" key={review.reviewId}>
+                      {review.reviewer}
+                    </li>
+                  ))}
+                </ul>
+              </DataGridCell>
+              <DataGridCell>
+                {Status(evaluation)} 
+              </DataGridCell>
+              <DataGridCell>
+                {/* //TODO:  - attach linking function to correct evaluation individual view once completed. */}
+
+                <Anchor
+                  href="#"
+                  id="detailsLink"
+                  onClick={() => {
+                    console.log(evaluation.apprentice.id);
+                  }}
+                >
+                  <span>View Details </span> &nbsp;
+                  <FileIcon
+                    id="linkingIcon"
+                    decorative={true}
+                    title="Link to Details of Evaluation"
+                  />
+                </Anchor>
+              </DataGridCell>
+            </DataGridRow>
+          ))}
+        </DataGridBody>
+      </DataGrid>
     </div>
   );
 };
