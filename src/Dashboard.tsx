ba3b8a67d0284b213655d2406b4c286bfade29bc
Elias Clark <eclark@twilio.com>
2022-07-01 11:30:44 -0400

    Added styling to header/footer + dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -21,22 +21,39 @@ import { Link } from "react-router-dom";
 import "array-sort";
 import { reverse } from "cypress/types/lodash";
 
-
-
-
-
 /**
- * This is the component 
- * @date 6/30/2022 - 2:33:43 PM
  *
- * @returns {*}
+ * This is the All Evaluations view, known as the Dashboard - since it is the main view all users see upon logging in.
+ * The content displayed in the dashboard table differs depending on the individual's userID, which is stored
+ * after logging in. The filtering Box on the left allows for string filtering Evaluations by name of Apprentice/Manager
+ * or by name of individual Reviewers.
+ *
  */
+
 const Dashboard = () => {
+  /**
+   *  CONSTANTS
+   *
+   * baseURL = the url of the API server that will be connected to via our Axios GET command.
+   * isDesc = a boolean toggle for the sorting.
+   * filter = string const, used for string filtering.
+   *
+   * evaluations = current array of data pulled from the API. filtered initially in the useEffect call,
+   *                changed actively w/ user-led filtering/sorting, and is displayed to user.
+   * savedEvaluations = a "backup" copy of the evaluations data. initially filtered like evaluations is in the useEffect call,
+   *                  but is never accessed directly - only used to reset the evaluations array after filtering.
+   * headerData= array of header titles for the dashboard DataGrid.
+   * arraySort = allows for us to sort data. Yay.
+   *
+   * (The currentUser constant is a temporary measure and will be deleted once we have full user Authorization parameters
+   * to play with. )
+   */
+
   const currentUser = {
     id: 2,
     name: "Jiminy Cricket",
-    role: 3
-  }
+    role: 3,
+  };
   const baseURL: string = "http://localhost:3000/";
   const [isDesc, setIsDesc] = useState(true);
   const [filter, setFilter] = useState("");
@@ -51,8 +68,8 @@ const Dashboard = () => {
       apprentice: { id: 420, name: "PLACEHOLDER" },
       manager: { id: 69, name: "PLACEHOLDER" },
       reviews: [
-        { reviewId: 100, reviewer: {id: 69, name: "PLACEHOLDER 01" }},
-        { reviewId: 101, reviewer: {id: 692, name: "PLACEHOLDER 02" }},
+        { reviewId: 100, reviewer: { id: 69, name: "PLACEHOLDER 01" } },
+        { reviewId: 101, reviewer: { id: 692, name: "PLACEHOLDER 02" } },
       ],
     },
   ]);
@@ -67,12 +84,12 @@ const Dashboard = () => {
       apprentice: { id: 420, name: "PLACEHOLDER" },
       manager: { id: 69, name: "PLACEHOLDER" },
       reviews: [
-        { reviewId: 100, reviewer: {id: 69, name: "PLACEHOLDER 01" }},
-        { reviewId: 101, reviewer: {id: 692, name: "PLACEHOLDER 02" }},
+        { reviewId: 100, reviewer: { id: 69, name: "PLACEHOLDER 01" } },
+        { reviewId: 101, reviewer: { id: 692, name: "PLACEHOLDER 02" } },
       ],
     },
   ]);
-   
+  const arraySort = require("array-sort");
 
   const HeaderData: any = [
     "Title",
@@ -82,42 +99,67 @@ const Dashboard = () => {
     "Reviewers",
   ];
 
+  /**
+   * LOAD FUNCTIONS
+   *
+   * useEffect calls the axios GET command at the /evaluations endpoint on page loading.
+   *
+   * */
+
   useEffect(() => {
     axios.get(baseURL + "evaluations").then((response) => {
       setEvaluations(
         response.data.filter((object: any) => {
           return (
-            object.apprentice.id === currentUser.id || object.manager.id === currentUser.id ||
-            (object.reviews.some((rev: any) => rev.reviewer.id === currentUser.id))
+            object.apprentice.id === currentUser.id ||
+            object.manager.id === currentUser.id ||
+            object.reviews.some(
+              (rev: any) => rev.reviewer.id === currentUser.id
+            )
           );
         })
       );
       setSavedEvaluations(
         response.data.filter((object: any) => {
           return (
-            object.apprentice.id === currentUser.id || object.manager.id === currentUser.id || (object.reviews.some((rev: any) => rev.reviewer.id === currentUser.id))
+            object.apprentice.id === currentUser.id ||
+            object.manager.id === currentUser.id ||
+            object.reviews.some(
+              (rev: any) => rev.reviewer.id === currentUser.id
+            )
           );
         })
       );
     });
   }, []);
 
-  const arraySort = require("array-sort");
+  /**
+   * FILTERING FUNCTIONS
+   *
+   * FilterEvaluations() - sorts data in columns in ascending/descending fashion, according to the toggle clicked.
+   *  @param param - string that matches the column's data type to be filtered, ie "id" or "date created".
+   *
+   * FilterNames() - filters all visible data according to the string inputed in the filtering box. This function is
+   * specific to the Apprentices and Managers, due to the hassle of filtering the nested Reviewer names.
+   *   @param str - string that is searched for using the 'includes' function. searches for anywhere in the string.
+   *
+   * FilterReviewerNames() - same as above, but specific to the Reviewers.
+   *
+   * All these functions return void - they just set the evaluations to what is sorted.
+   */
 
   function FilterEvaluations(param: string): void {
     let sortMePlease = [...evaluations];
     setIsDesc(!isDesc);
     let sortedEvals = arraySort(sortMePlease, param, { reverse: isDesc });
     setEvaluations(sortedEvals);
-    console.log(evaluations);
   }
 
-  function FilterNames(param: string): void {
-    if (param.length >= 1) {
+  function FilterNames(str: string): void {
+    if (str.length >= 1) {
       let filteredEvals: any = savedEvaluations.filter(
         (eva) =>
-          eva.apprentice.name.includes(param) ||
-          eva.manager.name.includes(param)
+          eva.apprentice.name.includes(str) || eva.manager.name.includes(str)
       );
       setEvaluations(filteredEvals);
     } else {
@@ -125,16 +167,23 @@ const Dashboard = () => {
     }
   }
 
-  function FilterReviewerNames(param: string): void {
-    if (param.length >= 1) {
+  function FilterReviewerNames(str: string): void {
+    if (str.length >= 1) {
       let filteredEvals: any = savedEvaluations.filter((eva) =>
-        eva.reviews.some((review) => review.reviewer.name.includes(param))
+        eva.reviews.some((review) => review.reviewer.name.includes(str))
       );
       setEvaluations(filteredEvals);
     } else {
       setEvaluations(savedEvaluations);
     }
   }
+
+  /**
+   * Status function - displays a box w/ a differing color & text depending on the status of that eval.
+   * @param evaluation = any one evaluation
+   * @returns Box w/ text showing status of evaluation.
+   *
+   */
 
   function Status(evaluation: any) {
     if (evaluation.status === "open") {
@@ -146,19 +195,38 @@ const Dashboard = () => {
     }
   }
 
+  function DisplayRole(roleID: number): string{
+    if (roleID === 1) {
+      return "Apprentice"
+    } else if (roleID ===2){
+      return "Reviewer"
+    } else if (roleID === 3){
+      return "Manager"
+    } else {
+      return "New God"
+    }
+  }
+
   return (
     <div id="dashboard">
       <div id="filterContainer">
+        <Box id="greetingBox">
+          Ahoy, <b>{currentUser.name}</b>!
+          <br /> <br />
+          Your id number is: <b>{currentUser.id}</b>
+          <br /> and your role is: <b>{DisplayRole(currentUser.role)}</b>.
+          <br /> 
+        </Box>
         <Box>
-          <Label htmlFor="filter">Filter by Apprentice/Manager: </Label>
+          <Label htmlFor="filterAppMan">Filter by Apprentice/Manager: </Label>
           <Input
-            id="filter"
-            name="filter"
+            id="filterAppMan"
+            name="filterAppMan"
             type="text"
             onChange={(e) => FilterNames(e.target.value)}
           />
           <br />
-          <Label htmlFor="filter">Filter by Reviewer: </Label>
+          <Label htmlFor="filterReviewer">Filter by Reviewer: </Label>
           <Input
             id="filterReviewer"
             name="filterReviewer"
@@ -240,7 +308,6 @@ const Dashboard = () => {
               <DataGridCell>
                 <Anchor
                   href="#"
-                 
                   id="detailsLink"
                   onClick={() => {
                     console.log("Linking to /evaluations/" + evaluation.id);
