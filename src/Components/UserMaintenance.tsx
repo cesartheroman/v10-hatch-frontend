3c48b6254de4a80888511fee4bd803ea25660e84
Cesar Roman <croman@twilio.com>
2022-07-08 17:00:31 -0400

    added simple styling for title and added more functionality for datagrid

--- src/Components/UserMaintenance.tsx
+++ src/Components/UserMaintenance.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import axios from "axios";
+import "../styles.css";
+import { useEffect, useState } from "react";
 import {
   DataGrid,
   DataGridHead,
@@ -8,16 +11,56 @@ import {
   DataGridCell,
   DataGridFoot,
 } from "@twilio-paste/core/data-grid";
+import { Heading } from "@twilio-paste/core";
 
 const UserMaintainance = () => {
+  const BASE_URL: string = "http://localhost:3000/users";
+  const HEADER_DATA: string[] = ["Name", "Role", "Email", "Update"];
+  const [users, setUsers] = useState([{}]);
+
+  const getUsers = async () => {
+    try {
+      const responseData = await axios.get(BASE_URL);
+      const { data: userData } = responseData;
+      setUsers(userData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  console.log("these are users: ", users);
   return (
-    <DataGrid aria-label="User Maintainance" striped>
-      <DataGridHead>
-        <DataGridRow>First Name</DataGridRow>
-        <DataGridRow>Last Name</DataGridRow>
-        <DataGridRow>Row</DataGridRow>
-      </DataGridHead>
-    </DataGrid>
+    <div>
+      <Heading as="h1" variant="heading10" id="userHeading">
+        All Users
+      </Heading>
+      <DataGrid aria-label="User Maintainance" striped>
+        <DataGridHead>
+          <DataGridRow>
+            {HEADER_DATA.map((header, idx) => (
+              <DataGridHeader key={idx}>{header}</DataGridHeader>
+            ))}
+          </DataGridRow>
+        </DataGridHead>
+        <DataGridBody>
+          {users.length <= 1 ? (
+            <DataGridRow>
+              <DataGridCell>{"text"}</DataGridCell>
+            </DataGridRow>
+          ) : (
+            users.map((user, idx) => {
+              <DataGridRow key={idx}>
+                <DataGridCell>{user}</DataGridCell>
+              </DataGridRow>;
+            })
+          )}
+        </DataGridBody>
+      </DataGrid>
+    </div>
   );
 };
 
